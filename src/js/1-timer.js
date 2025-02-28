f2cf98fc30e76ba0fa6f62e453df0f2016428136
Suzy-Lana777 <reutasvetlana382@gmail.com>
2025-02-28 19:18:43 +0200

    part 2 snackbar

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,55 @@
+// import flatpickr from "flatpickr";
+// import iziToast from "izitoast";
+// import "izitoast/dist/css/iziToast.min.css";
+// import "flatpickr/dist/flatpickr.min.css";
 
+
+
+// const refs = {
+//   startBtn: document.querySelector('datetime-picker'),
+//   stopBtn: document.querySelector('[data-action-stop]'),
+//   clockface: document.querySelector('.js-clockface'),
+// };
+
+// const timer = {
+//   intervalId: null,
+//   initTime: null,
+
+//   start() {
+//     console.log('START');
+//     this.initTime = new Date('04.02.2025');
+//     this.intervalId = setInterval(() => {
+//       this.tick();
+//     }, 1000);
+
+//     refs.startBtn.disabled = true;
+//     refs.stopBtn.disabled = false;
+//   },
+
+//   stop() {
+//     console.log('STOP');
+//     refs.startBtn.disabled = false;
+//     refs.stopBtn.disabled = true;
+//     clearInterval(this.intervalId);
+//   },
+
+//   tick() {
+//     const currentTime = Date.now();
+//     const ms = this.initTime - currentTime;
+//     const time = getTimeComponents(ms);
+//     const str = timeToSTR(time);
+//     refs.clockface.textContent = str;
+
+//     if (ms < 1000) {
+//       this.stop();
+//     }
+//   },
+// };
+
+// refs.startBtn.addEventListener('click', () => {
+//   timer.start();
+// });
+
+// refs.stopBtn.addEventListener('click', () => {
+//   timer.stop();
+// });
